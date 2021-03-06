733777b131c34560f0c242b008ed70045ced7dfb
Phillip Johnsen <johphi@gmail.com>
2021-03-06 21:06:01 +0100

    Add `action.yml` and `index.js` as main entry point
    This is the actual moment where this project suddenly becomes a GitHub
    Action that can be installed in GitHub repositories.

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const github = require('@actions/github')
+const core = require('@actions/core')
+const nodeRepo = require('./lib/node-repo')
+
+async function run () {
+  try {
+    const token = core.getInput('repo-token', { required: true })
+    const configPath = core.getInput('configuration-path', { required: true })
+    const pullRequest = github.context.payload.pull_request
+
+    if (!pullRequest) {
+      throw new Error('Could not resolve pull request number, is Action triggered by something else than a pull request?')
+    }
+
+    const client = github.getOctokit(token)
+    const { owner, repo } = github.context.repo
+    const prId = pullRequest.number
+    const baseBranch = pullRequest.base.ref
+    const configAsString = await fetchConfig(client, owner, repo, configPath)
+
+    await nodeRepo.resolveLabelsThenUpdatePr({
+      baseBranch,
+      client,
+      configAsString,
+      owner,
+      repo,
+      prId
+    })
+  } catch (error) {
+    core.error(error)
+    core.setFailed(error.message)
+  }
+}
+
+async function fetchConfig (
+  client,
+  owner,
+  repo,
+  filepath
+) {
+  const response = await client.repos.getContents({
+    owner,
+    repo,
+    path: filepath,
+    ref: github.context.sha
+  })
+
+  return Buffer.from(response.data.content, response.data.encoding).toString()
+}
+
+run()
